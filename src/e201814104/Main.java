e1ca46f76bec705900c4a2d08d155fe2be6ddb0b
Kim, Jaeseong <06zzkimzz06@gmail.com>
2022-09-26 17:06:07 +0900

    Main.java수정 #4

--- src/e201814104/Main.java
+++ src/e201814104/Main.java
@@ -1,7 +1,13 @@
 package e201814104;
 
 public class Main {
+
+	static void say(){
+		System.out.println("main");
+	}
+
 	public static void main(String[] args) {
+		say();
 		Hello hello = new Hello();
 		hello.say("hello world");
 	}
